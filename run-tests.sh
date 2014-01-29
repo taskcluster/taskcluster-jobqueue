3e07093e8717dcbdf7ad2eb6ea69f9cbeaf26686
Dan Minor <dminor@mozilla.com>
2014-01-29 16:18:18 -0500

    Fix stresstest and add to run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
+# run unit tests
 python3.3 src/jobqueue.py --port 8315 --reset &
 PID=$!
 sleep 2
 PYTHONPATH=src python3.3 -m unittest discover -p -s tests "*_test.py"
+kill $PID
+
+# run stresstest 
+python3.3 src/jobqueue.py --reset &
+PID=$!
+sleep 2
+./stresstest/stresstest.py
+if [ $? -eq 0 ]
+then echo 'OK'
+fi
 kill $PID
